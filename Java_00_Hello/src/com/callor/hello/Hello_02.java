9e407e03465fb7b243bfefb21860c0565da40e5e
num5268 <num5268@gmail.com>
2021-03-16 15:43:18 +0900

    Hello_02

--- Java_00_Hello/src/com/callor/hello/Hello_02.java
+++ Java_00_Hello/src/com/callor/hello/Hello_02.java
@@ -3,6 +3,16 @@ package com.callor.hello;
 	public class Hello_02 {
 		public static void main(String[]args) {
 		
+			System.out.println(30+40);
+			System.out.println(30-40);
+			System.out.println(30*40);
+			System.out.println(30/40);
+			System.out.println(30 % 40);
+			System.out.println(1 % 2);
+			System.out.println(2 % 2);
+			System.out.println(3 % 2);
+			System.out.println(4 % 2);
+			System.out.println(62.5 % 12);
 	}
 
 }
---
+++ Java_00_Hello/src/com/callor/hello/Hello_03.java
@@ -0,0 +1,19 @@
+package com.callor.hello;
+
+public class Hello_03 {
+
+	public static void main(String[] args) {
+		System.out.println(30+40);
+		System.out.println(30-40);
+		System.out.println(30*40);
+		System.out.println(30/40);
+		System.out.println(30 % 40);
+		System.out.println(1 % 2);
+		System.out.println(2 % 2);
+		System.out.println(3 % 2);
+		System.out.println(4 % 2);
+		System.out.println(62.5 % 12);
+
+	}
+
+}
