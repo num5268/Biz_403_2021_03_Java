0d8382048b86311dcd87a166636469973aad9207
num5268 <num5268@gmail.com>
2021-03-17 10:04:03 +0900

    연산복습

---
+++ Java_000_Hello/src/com/callor/hello/Numbers.java
@@ -0,0 +1,21 @@
+package com.callor.hello;
+
+public class Numbers {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("33+2="+ (33+2));
+		System.out.println("33-2="+ (33-2));
+		System.out.println("33x2="+ (33*2));
+		System.out.println("33/2="+ (33/2));
+		System.out.println("33%2="+ (33%2));
+	
+		System.out.println("====================");
+		System.out.print("33+2=");
+		System.out.println(33+2);
+		System.out.print("33-2=");
+		System.out.println(33-2);
+		
+	}
+
+}
