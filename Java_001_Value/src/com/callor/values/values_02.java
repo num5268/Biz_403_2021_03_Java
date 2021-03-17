ef51407baedf0948d2f719d7697dc78fcd5c6c3d
num5268 <num5268@gmail.com>
2021-03-17 11:50:38 +0900

    Values

---
+++ Java_001_Value/src/com/callor/values/values_02.java
@@ -0,0 +1,12 @@
+package com.callor.values;
+
+public class values_02 {
+
+	public static void main(String[]args) {
+		System.out.println("원의 넓이와 둘레를 구하시오");
+		System.out.println("원의 반지름값은 23");
+		System.out.println("원의 둘레:"+ (23 * 2 * 3.14));
+		System.out.println("원의 넓이:"+ (23 * 23 * 3.14));
+		
+	}
+}
