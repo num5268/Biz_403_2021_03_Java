b67a3bba172b6faf8f84e96df3fb3ad180b8b234
num5268 <num5268@gmail.com>
2021-03-24 16:40:03 +0900

    03-24END

---
+++ Java_006_Application/src/com/callor/apps/App_12.java
@@ -0,0 +1,15 @@
+package com.callor.apps;
+
+import com.callor.apps.service.LinesService;
+
+public class App_12 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(LinesService.dLine);
+		System.out.println("국어\t 영어\t 수학");
+		System.out.println(LinesService.sLine);
+		
+	}
+
+}
